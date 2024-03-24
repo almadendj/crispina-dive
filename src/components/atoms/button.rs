f1fd7e1f0a9d36d6a111a208a0e1b98048d26fb0
almaden <almadendj@gmail.com>
2024-03-24 12:49:28 +0800

    add daisyui

--- src/components/atoms/button.rs
+++ src/components/atoms/button.rs
@@ -3,6 +3,6 @@ use yew::prelude::*;
 #[function_component]
 pub fn Button() -> Html {
     html! {
-        <button class="font-semibold border border-gray-500 px-4 py-2 rounded-xl">{"Hello World!"}</button>
+        <button class="btn btn-neutral">{"Hello World!"}</button>
     }
 }
